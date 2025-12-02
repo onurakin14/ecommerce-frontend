2c1f0703a6fae104c93c5d0e2f2b10cde673d8eb
Onur Akın <onur.akin@linktera.com>
2025-12-02 13:24:25 +0300

    Initial commit

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import cartReducer from "./cartSlice";  // Sonra ekleyeceğiz
+// import userReducer from "./userSlice";  // Sonra ekleyeceğiz
+
+export const store = configureStore({
+  reducer: {
+    // cart: cartReducer,
+    // user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
