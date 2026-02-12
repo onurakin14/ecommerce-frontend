c7979cf38584fa70fa71ba1fadaf78dc40f5c52c
mehmetkoroglu <85640605+mehmetkoroglu@users.noreply.github.com>
2026-02-12 23:58:03 +0300

    backend: get userid for cart

--- backend/src/app/api/users/[userId]/carts/route.ts
+++ backend/src/app/api/users/[userId]/carts/route.ts
@@ -2,13 +2,14 @@ import { connectDB } from "@/lib/db/mongodb";
 import Cart from "@/lib/models/Cart";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: Request, context: { params: Promise<{ userId: string }> }) {
     try {
         await connectDB();
+        const { userId } = await context.params;
 
         const carts = await Cart.find();
-        return NextResponse.json({ carts });
 
+        return NextResponse.json({ message: `User ID is ${userId}` });
     } catch (err: any) {
         return NextResponse.json({ error: err.message }, { status: 500 });
     }
